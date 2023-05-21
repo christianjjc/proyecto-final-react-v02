2f4f2d6180d73516f72641f3233dda6aa08b8948
christianjjc <christianjjc@gmail.com>
2023-05-21 12:17:44 -0500

    Moviento componentes a la carpeta COMPONENTS ok

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import NavBar from './NavBar/NavBar';
-import ContainerAll from './ContainerAll/ContainerAll';
-import ItemListContainer from './ItemListContainer/ItemListContainer';
+import NavBar from './components/NavBar/NavBar'
+import ContainerAll from './components/ContainerAll/ContainerAll';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import React from 'react';
 
 function App() {
-
    return (
-
-   
-
         <>
           <ContainerAll>
             <NavBar />
             <ItemListContainer greeting='Mi Catálogo de Productos' />
           </ContainerAll>
         </>
-    
-
-/*     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */
   ); 
 }
 
