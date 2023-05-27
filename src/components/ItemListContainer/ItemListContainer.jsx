f6de3ee0987d01b5a2184195da3acd69fb21441b
christianjjc <christianjjc@gmail.com>
2023-05-27 11:54:00 -0500

    preparando para crear una funcion que escoja si jalar datos de firestore, de axios o por fetch

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -7,14 +7,57 @@ import { putArrayInLocalS } from '../../helpers/utilitarios';
 
 import { collection, getDocs, getFirestore } from 'firebase/firestore'  //conexion a FIRESTORE de GOOGLE
 
+/* const ItemListContainer = ({ titulo }) => {
+    const [data, setData] = useState([]);
+    const { idCategoria } = useParams();
+    const URL_API = 'https://raw.githubusercontent.com/christianjjc/proyecto-final-react-v02/main/src/components/Item/json/tblProductos.json'
+
+    const obtenerDatos = ()=>{
+        axios.get(URL_API)
+        .then((response)=>{
+            const array = response.data;
+            let arrayFiltrado = [];
+            if (idCategoria) {
+                arrayFiltrado = array.filter((item)=>{
+                    return item.idCategoria.some(categoria => categoria.includes(idCategoria))
+                })
+            } else {
+                arrayFiltrado = array;
+            }
+            putArrayInLocalS(arrayFiltrado, 'animes');
+            setData(arrayFiltrado); 
+        })
+        .catch((err)=>{console.log(err)})
+    }
+
+    useEffect(()=>{
+        obtenerDatos();
+    },[idCategoria]);
+
+    return (
+        <div className='row itemListContainer'>
+            <div className="col-12">
+                <div className="row">
+                    <div className='text-center col-12'>
+                        <h1>{`${titulo} ${idCategoria || ''}`}</h1>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div  className='col-12'>
+                        <ItemList item={data} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}; */
+
+
 const ItemListContainer = ({ titulo }) => {
     const [data, setData] = useState([]);
     const { idCategoria } = useParams();
-    //const URL_API = 'https://raw.githubusercontent.com/christianjjc/proyecto-final-react-v02/main/src/components/Item/json/tblProductos.json'
 
     const obtenerDatos = ()=>{
-        //axios.get(URL_API)
-        //.then((response)=>{
         const db = getFirestore();
         const itemsCollection = collection(db,'animes');
         getDocs(itemsCollection)
@@ -60,5 +103,6 @@ const ItemListContainer = ({ titulo }) => {
         </div>
     );
 };
+
 
 export default ItemListContainer;
